5805ab49381c11043ccf9dc9d1ecd00d2ab45a01
Jason Vance <jbvance@gmail.com>
2017-11-21 19:11:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,13 @@ def batch_badge_creator(names)
   names.each do |name|
     newArray.push("Hello, my name is #{name}.")
   end
+  newArray
+end
+
+def assign_rooms(names)
+  newArray = []
+  names.each_with_index do |name, index|
+    newArray.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+  newArray
 end
